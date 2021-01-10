b8755cf3e94f573ccbb3b2995fe2d6013126f7f9
Live session user <jose@jpinto.com.br>
2021-01-10 09:26:20 -0300

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import Routes from './Routes';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
+      <Routes />
     </>
   );
 }
