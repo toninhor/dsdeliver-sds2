0816aac382d55d7735eb24871aa39952b6c299e8
Live session user <jose@jpinto.com.br>
2021-01-09 22:46:50 -0300

    Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <Navbar />
     </div>
   );
 }
