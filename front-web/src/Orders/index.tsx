d701472237c5ee842bb549949d903b85ef126277
Live session user <jose@jpinto.com.br>
2021-01-10 14:23:10 -0300

    Read product list

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
 import './styles.css';
 import StepsHeader from './StepsHeader';
-//import { ReactComponent as Logo } from './logo.svg';
+import ProductsLists from './ProductsLists';
+import { Product } from './types';
+import { fetchProducts } from './apis';
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    },[])
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsLists products={products}/>
         </div>
     )
 }
