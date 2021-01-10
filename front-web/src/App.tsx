4052e91aa9b912f5f907f4a90512912788969003
Live session user <jose@jpinto.com.br>
2021-01-09 23:20:50 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
