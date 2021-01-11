dd4a9f1e90d3ab2d213f6c859a78864d752a4ef0
Live session user <jose@jpinto.com.br>
2021-01-11 00:09:45 -0300

    Add env vars

--- front-web/src/Orders/apis.ts
+++ front-web/src/Orders/apis.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:8080';
+const API_URL = process.env.REACT_APP_API_URL; //'http://localhost:8080'
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX; //Só funciona com o token colado não com a variável global
 
 export function fetchProducts() {
