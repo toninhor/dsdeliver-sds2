0ceb13706ebdce06e8fb6221e8939cfb8f6a0ee1
José Antonio <61567500+toninhor@users.noreply.github.com>
2026-07-04 00:00:41 -0300

    Add WebmailRedirect import to Routes component

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Orders from "./Orders";
 import Home from "./Home";
 import Navbar from "./Navbar";
+import WebmailRedirect from "./Webmail";
 
 function Routes() {
     return (
