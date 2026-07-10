f7aab0674a3b3163b52d820b416070186b080427
José Antonio <61567500+toninhor@users.noreply.github.com>
2026-07-10 19:52:42 -0300

    Implement redirect to Hostinger mail after 2 seconds
    Added a timeout to redirect to Hostinger mail after 2 seconds.

--- front-web/src/Webmail/WebmailRedirect.tsx
+++ front-web/src/Webmail/WebmailRedirect.tsx
@@ -1,9 +1,16 @@
 import { useEffect } from "react";
 
 export default function WebmailRedirect() {
+  // Redirect after 2 secconds.
+  setTimeout(() => {
+    RedirectMail();
+  }, 2000);
+
+  return <p>Redirecionando para <b>Hostinger...</b></p>;
+}
+
+function RedirectMail() {
   useEffect(() => {
     window.location.href = "https://mail.hostinger.com";
   }, []);
-
-  return <p>Redirecionando para <b>Hostinger...</b></p>;
 }
